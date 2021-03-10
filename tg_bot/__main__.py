e852aa598d74d2e9d77ce8c16269a2da6269de32
HITMAN | KARTIK <74542005+kartikrajofficial0773h@users.noreply.github.com>
2021-03-10 23:07:08 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -146,7 +146,7 @@ def start(bot: Bot, update: Update, args: List[str]):
                                                                                    [InlineKeyboardButton(text="ADD KINGBOT TO YOUR GROUP",
                                                                        url="t.me/{}?startgroup=true".format(bot.username)),
                                                                                    InlineKeyboardButton(text="Source Code",
-                                                                       url="https://github.com/Kartikrajofficial0773h/KINGBOT")
+                                                                       url="t.me/KINGBOTOFFICIAL")
                                                                                  ]]))
 
     else:
